a40b70ca05b80f7c60729504f54a5a6422bbc1f6
Ferdinand II Dangaran <160829247+Dangaran-FerdinandII-BSIT32E2@users.noreply.github.com>
2024-06-30 21:56:56 +0800

    finished code

---
+++ ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectedApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            
+            var userInfo = new
+            {
+                Name = "Ferdinand II Dangaran",
+                Section = "BSIT32E2",
+                Course = "Information Technology"
+            };
+
+            var funFacts = new[]
+            {
+                "I want to be a Fullstack Developer.",
+                "I am good at Valorant.",
+                "I love to watch anything.",
+            };
+
+            return Ok(new { UserInfo = userInfo, FunFacts = funFacts });
+        }
+    }
+}
